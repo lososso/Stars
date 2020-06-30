fa0f7bb5db4095f6479670b7a8669857ec385172
msos <marcsoscambras@gmail.com>
2020-06-30 21:34:15 +0200

    First raw data recieved

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = 'https://es.wikipedia.org/wiki/Anexo:Estrellas_m%C3%A1s_cercanas'
+URL = 'http://web.archive.org/web/20050923194412/http://www.chara.gsu.edu/RECONS/TOP100.htm'
 page = requests.get(URL)
-print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+text = (soup.get_text())
+a = text.split('\n')
+list_stars = []
+for i in range(62, (len(a)-85)):
+    list_stars.append((a[i]))
+for elements in list_stars:
+    a = elements.split("\t")
